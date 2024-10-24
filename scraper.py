aaa54bb08b72cec510563e9b7ea240dc175fee84
emelib <emelib@uci.edu>
2024-10-24 00:38:12 -0700

    Started the is valid function by creating a set of valid url domains and then checking if the url being passed into the method has a valid scheme or domain.

--- scraper.py
+++ scraper.py
@@ -2,8 +2,11 @@ import re
 from urllib.parse import urlparse
 
 def scraper(url, resp):
-    links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    if resp.status = 200:
+        links = extract_next_links(url, resp)
+        return [link for link in links if is_valid(link)]
+    else:
+        []
 
 def extract_next_links(url, resp):
     # Implementation required.
@@ -21,10 +24,14 @@ def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
+    valid_domains = ("ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu", "today.uci.edu/department/information_computer_sciences" )
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
+        for domain in valid_domains:
+            if parsed.netloc.endswith(domain):
+                return True
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
@@ -34,7 +41,6 @@ def is_valid(url):
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
-
     except TypeError:
         print ("TypeError for ", parsed)
         raise
